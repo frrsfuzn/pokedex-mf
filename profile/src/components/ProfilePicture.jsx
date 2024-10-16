49017f6a4ec97b53f06c0e3ac8f5872e9b0c2351
frrsfuzn <mochfarrasfauzan@gmail.com>
2024-10-16 22:11:42 +0700

    handle large file

--- profile/src/components/ProfilePicture.jsx
+++ profile/src/components/ProfilePicture.jsx
@@ -1,24 +1,44 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 import { FaPencilAlt } from "react-icons/fa";
-import { getBase64 } from '../utils/fileUtils';
+import { getBase64 } from "../utils/fileUtils";
+import toast from "host/toast";
 
 function ProfilePicture({ url, onSuccessEdit }) {
   const inputRef = useRef(null);
   const onClickEdit = () => {
     inputRef.current.click();
-  }
+  };
 
   const handleFileInput = (e) => {
-    getBase64(e.target.files[0], onSuccessEdit);
-  }
+    const file = e.target.files[0];
+    if (file) {
+      console.log(file?.size);
+      if (file?.size < 1000000) {
+        getBase64(file, onSuccessEdit);
+      } else {
+        toast.error("File is too large! Maximum size is 1MB");
+      }
+    }
+  };
 
   return (
-    <div className='rounded-full aspect-square overflow-hidden w-28 relative'>
+    <div className="rounded-full aspect-square overflow-hidden w-28 relative">
       <img className="absolute w-full" src={url} />
-      <button onClick={onClickEdit} className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center hover:opacity-60 bg-slate-50 opacity-0 transition-opacity'><FaPencilAlt /></button>
-      <input ref={inputRef} type='file' className='hidden' accept="image/*" onChange={handleFileInput} />
+      <button
+        onClick={onClickEdit}
+        className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center hover:opacity-60 bg-slate-50 opacity-0 transition-opacity"
+      >
+        <FaPencilAlt />
+      </button>
+      <input
+        ref={inputRef}
+        type="file"
+        className="hidden"
+        accept="image/*"
+        onChange={handleFileInput}
+      />
     </div>
-  )
+  );
 }
 
-export default ProfilePicture
+export default ProfilePicture;
